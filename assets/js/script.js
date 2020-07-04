3bdb659033cc581e8e22a924c712bb78fcf6ee50
PasdeloupNathan <gamee8678@gmail.com>
2020-07-04 12:20:15 +0200

    init repo

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+function slide1() {
+    x = document.getElementById('mesI')
+    x.setAttribute('width', '100%')
+    x.setAttribute('height', '100%')
+    x.setAttribute("src", 'https://previews.123rf.com/images/jvdwolf/jvdwolf1707/jvdwolf170700027/82009840-excavatrice-%C3%A0-roue-%C3%A0-godets-dans-une-mine-%C3%A0-ciel-ouvert-%C3%A0-charbon-brun-.jpg');
+
+}
+
+function slide2() {
+    x = document.getElementById('mesI')
+    x.setAttribute('width', '100%')
+    x.setAttribute('height', '100%')
+    x.setAttribute("src", 'https://previews.123rf.com/images/saoirse2010/saoirse20101605/saoirse2010160500008/58303971-de-nombreux-godets-d-excavatrices-g%C3%A9antes-pour-l-extraction-du-sable-de-la-carri%C3%A8re-.jpg');
+}
+
+
+function slide3() {
+    x = document.getElementById('mesI')
+    x.setAttribute('width', '100%')
+    x.setAttribute('height', '100%')
+    x.setAttribute("src", 'https://previews.123rf.com/images/saoirse2010/saoirse20101605/saoirse2010160500011/58303965-de-nombreux-godets-d-excavatrices-g%C3%A9antes-pour-l-extraction-du-sable-de-la-carri%C3%A8re-vue-arri%C3%A8re.jpg');
+}
+
+function slide4() {
+    x = document.getElementById('mesI')
+    console.log(x)
+    x.setAttribute('width', '100%')
+    x.setAttribute('height', '100%')
+    x.setAttribute("src", 'https://previews.123rf.com/images/jvdwolf/jvdwolf1706/jvdwolf170600264/81287394-enorme-excavatrice-%C3%A0-roue-%C3%A0-godets-dans-une-industrie-mini%C3%A8re-%C3%A0-charbon-%C3%A0-ciel-ouvert.jpg');
+}
+
+
+function slide5() {
+    x = document.getElementById('mesI')
+    x.setAttribute('width', '100%')
+    x.setAttribute('height', '100%')
+    x.setAttribute("src", 'https://img.pixers.pics/pho_wat(s3:700/FO/52/54/58/84/700_FO52545884_c708fb4def7f76853fcf4dae34db737c.jpg,700,467,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,417,jpg)/papiers-peints-une-excavatrice-geante-a-roue-a-godets-dans-une-mine-de-lignite.jpg');
+}
+
+function base() {
+    x = document.getElementById('mesI')
+    x.setAttribute('width', '100%')
+    x.setAttribute('height', '100%')
+    x.setAttribute("src", 'https://previews.123rf.com/images/jvdwolf/jvdwolf1707/jvdwolf170700027/82009840-excavatrice-%C3%A0-roue-%C3%A0-godets-dans-une-mine-%C3%A0-ciel-ouvert-%C3%A0-charbon-brun-.jpg');
+
+}
